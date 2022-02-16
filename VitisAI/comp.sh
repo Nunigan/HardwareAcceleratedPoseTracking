8d1bd3dbf689c6ef3fb6c16113111d2ea96479e3
Nunigan <michael.schmid2@ost.ch>
2022-02-16 15:23:44 +0100

    added gitignore anmd prebuilt folder

--- VitisAI/comp.sh
+++ VitisAI/comp.sh
@@ -1,6 +1,6 @@
 vai_c_tensorflow \
     --frozen_pb  ./quantize/quantize_eval_model.pb \
-    --arch       arch_vck.json \
+    --arch       arch_kv.json \
     --output_dir ./compile/ \
-    --net_name   own_network_256_bigdpu \
+    --net_name   own_network_256_KV260 \
     --option    "{'dump': 'all'}"
