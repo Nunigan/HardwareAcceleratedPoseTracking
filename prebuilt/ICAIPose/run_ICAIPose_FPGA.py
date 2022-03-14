7d01c35b488b8223db2d4d3821235c2134f73605
Nunigan <michael.schmid2@ost.ch>
2022-03-14 14:39:41 +0100

    added HDMI support

--- prebuilt/ICAIPose/run_ICAIPose_FPGA.py
+++ prebuilt/ICAIPose/run_ICAIPose_FPGA.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 09:35:29 2021
+
+@author: nunigan
+"""
+
 from ctypes import *
 from typing import List
 import cv2
@@ -14,6 +20,9 @@ import time
 import sys
 import argparse
 import queue
+import time
+import gc
+
 
 from pose_parameter import PoseParameter
 from imutils.video import FPS
@@ -26,7 +35,7 @@ def taskCapture(inputId,queueIn):
 
     global bQuit
 
-    #print("[INFO] taskCapture : starting thread ...")
+
 
     # Start the FPS counter
     fpsIn = FPS().start()
@@ -34,6 +43,7 @@ def taskCapture(inputId,queueIn):
     # Initialize the camera input
     print("[INFO] taskCapture : starting camera input ...")
     cam = cv2.VideoCapture(inputId, cv2.CAP_GSTREAMER)
+
     if not (cam.isOpened()):
         print("[ERROR] taskCapture : Failed to open camera ", inputId )
         exit()
@@ -42,7 +52,7 @@ def taskCapture(inputId,queueIn):
         # Capture image from camera
         ret,frame = cam.read()
         R, G, B = cv2.split(frame)
-        frame = cv2.merge([R, G, B])        # Update the FPS counter
+        frame = cv2.merge([R, G, B])      
         fpsIn.update()
 
         # Push captured image to input queue
@@ -75,34 +85,41 @@ def taskWorker(worker,dpu,queueIn,queueOut):
         # Push processed image to output queue
         queueOut.put(res)
 
+    # Stop the face detector
+
     queueIn.put(frame)
 
-    print("[INFO] taskWorker[",worker,"] : exiting thread ...")
+
     
     
 def taskDisplay(queueOut):
 
     global bQuit
+    start = time.time()
 
     print("[INFO] taskDisplay : starting thread ...")
 
     # Start the FPS counter
     fpsOut = FPS().start()
 
+    # out = cv2.VideoWriter('appsrc ! videoconvert ! video/x-raw, format=NV12! kmssink driver-name=xlnx plane-id=39 sync=false fullscreen-overlay=true',0, 30.0, (width, height))
     while not bQuit:
         # Pop processed image from output queue
         frame = queueOut.get()
+	
+        frame = cv2.flip(frame, 1)
+        frame = cv2.resize(frame, (1024, 1024), interpolation=cv2.INTER_LINEAR)
+        frame = cv2.copyMakeBorder(frame, height//2-512, height//2-512, width//2-512, width//2-512, cv2.BORDER_CONSTANT, value=[0,0,0])
 
-        # Display the processed image
-        cv2.imshow("ICAIPose", frame)
+        cv2.imshow('ICAIPose',frame)
+        key = cv2.waitKey(1) & 0xFF
 
         # Update the FPS counter
         fpsOut.update()
 
-        # if the `q` key was pressed, break from the loop
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
+
+        # if time.time() -start > 300:
+        #     break
         
     bQuit = True
 
@@ -129,12 +146,15 @@ def get_child_subgraph_dpu(graph: "Graph") -> List["Subgraph"]:
     ]
     
 def main(argv):
-
+    global width
+    global height
     global bQuit
     bQuit = False
     inputId = 'mediasrcbin media-device=/dev/media0 v4l2src0::io-mode=dmabuf v4l2src0::stride-align=256  ! video/x-raw, width=256, height=256, format=NV12, framerate=30/1 ! videoconvert! appsink'
     threads = int(argv[1])
     ICAIPose_xmodel = argv[2]
+    width = int(argv[3])
+    height = int(argv[4])
     ICAIPose_graph = xir.Graph.deserialize(ICAIPose_xmodel)
     ICAIPose_subgraphs = get_child_subgraph_dpu(ICAIPose_graph)
     assert len(ICAIPose_subgraphs) == 1 # only one DPU kernel
@@ -165,7 +185,15 @@ def main(argv):
 
     # Cleanup VART API
     del all_dpu_runners
+    all_dpu_runners = None
+    gc.collect()
+    del threadAll
+    threadAll = None
+    gc.collect()
 
-
+    
+    
 if __name__ == "__main__":
-    main(sys.argv)
+    while True:
+        main(sys.argv)
+
